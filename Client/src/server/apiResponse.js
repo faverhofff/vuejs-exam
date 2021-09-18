0d9411a194995d59f70ca63721aa5b60c6795f2b
Sandy Averhoff <zandybierhoff@gmail.com>
2021-09-17 21:19:44 -0400

    move server code

--- Client/src/server/apiResponse.js
+++
@@ -1,8 +0,0 @@
-exports.apiResponse = (results, errors, statusCode) => {
-    if(errors == null) errors = ''
-    return {     
-        errors: errors,
-        status: statusCode,
-        data: results        
-    };
-};
