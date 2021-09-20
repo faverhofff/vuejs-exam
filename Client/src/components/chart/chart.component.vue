eb1f39dc0117e5b5cd360d4d10871d6cc6e9b19a
Frank Averhoff <frank.averhoff@onetree.com>
2021-09-20 12:28:35 -0300

    update

--- Client/src/components/chart/chart.component.vue
+++ Client/src/components/chart/chart.component.vue
@@ -4,12 +4,16 @@
       <h2>Timing Analysis</h2>
     </div>
     
+    <div class="col-12 pb-2">&nbsp;</div>
+
     <div v-if="measure == undefined" class="row d-flex justify-content-center">
         <h4>There'n site requested</h4>
     </div>  
 
     <div v-if="measure != undefined" class="row d-flex justify-content-center">
-      <div class="col-lg-6 text-center ratio-label page-load">
+      
+      <div class="box" :style="{ 'linear-gradient': '(10deg,transparent 50%,#fff 0) top/100% 200%, radial-gradient(farthest-side at bottom,#fff calc(100% - 20px),transparent 0), linear-gradient(to right, green , yellow , red);' }" ></div>
+      <div v-if="measure != undefined" class="justify-content-center box-info">
         <div>
           <h3>{{ measure?.pageLoadScore }}</h3>
           <h2>{{ message }}</h2>
@@ -18,7 +22,10 @@
         <h1>{{ measure?.pageLoadTime }}s</h1>
       </div>
 
-      <div class="col-lg-6 text-center ratio-label first-interaction">
+      <div class="col-12 pb-4">&nbsp;</div>
+
+      <div class="box"></div>
+      <div class="col-lg-6 text-center ratio-label first-interaction box-info">
         <div>
           <h3>{{ measure?.firstInteractionScore }}</h3>
           <h2>{{ message }}</h2>
@@ -34,6 +41,7 @@
 import { ChronosModel } from '@/core/model/chronos.model';
 import { requestService } from '@/core/services/request.service';
 import BaseComponent from '../base/base.component';
+
 
 export default class ChartComponent extends BaseComponent {
   public measure!: ChronosModel;
