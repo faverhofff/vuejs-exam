5836625805d861b6a9fab61e305f82e194217cc3
Sandy Averhoff <zandybierhoff@gmail.com>
2021-09-21 07:03:38 -0400

    remove old version

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import { refreshHeaderTokenInterceptor } from './interceptors/axios-interceptor-header-token'
-
-createApp(App).mount('#app')
-
-refreshHeaderTokenInterceptor
--- src/server/apiResponse.js
+++
@@ -1,7 +0,0 @@
-exports.apiResponse = (results, errors, statusCode) => {
-    return {     
-        errors: errors,
-        status: statusCode,
-        data: results        
-    };
-};
